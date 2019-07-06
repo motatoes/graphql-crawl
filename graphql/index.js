f6f5b24e9428bb3bc602dbb0c75134b2a4c29693
motatoes <moe.habib9@gmail.com>
2019-07-06 18:17:04 +0100

    allow remote playground

--- graphql/index.js
+++ graphql/index.js
@@ -41,7 +41,7 @@ const resolvers = {
 // In the most basic sense, the ApolloServer can be started
 // by passing type definitions (typeDefs) and the resolvers
 // responsible for fetching the data for those types.
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ typeDefs, resolvers, listen: "0.0.0.0" });
 
 // This `listen` method launches a web-server.  Existing apps
 // can utilize middleware options, which we'll discuss later.
